2f62c55f92d1f1e742c3471345711ebe776a5e06
Admin <admin@gmail.com>
2025-02-26 09:24:23 -0800

    message

---
+++ login.js
@@ -0,0 +1,16 @@
+const user = "User"
+const password = "Password"
+
+
+function login(){
+    let User = "User"
+    let Password = "Password"
+
+    if(Password == password && User == user ){
+        console.log("login successful")
+    }else{
+        console.log("wrong  credentials")
+    }
+}
+
+login()

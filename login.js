e2e0d50c2e7f040a546843abb44179225013352f
Admin <admin@gmail.com>
2025-02-27 16:50:16 -0800

    message

--- login.js
+++ login.js
@@ -1,18 +1,18 @@
+const loginButton = document.getElementById("btn")
+const username = "User"
+const password = "Password"
+const email = "Email"
 
 
-const form =document.getElementById('form');
-const username =document.getElementById('username');
-const password =document.getElementById('password');
-form.addEventListener('submit',e =>{
-    e.preventDefault();
-    validateInputs();
-});
+loginButton.addEventListener("click", login = () => {
+    let Username = "User"
+    let Password = "Password" 
 
-const setError = (element,message) =>{
-    const inputControl = element.parentElement;
-}
-const validateInputs = () =>{
-    const usernameValue = username.value.trim();
-    const passwordValue = password.value.trim();
+    if(Password == password && User == user ){
+        console.log("login successful")
+    }else{
+        console.log("wrong  credentials")
+ 
+}}) 
 
-};
+
